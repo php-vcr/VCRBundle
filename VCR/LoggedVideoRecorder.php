9d7795f298bf2ade35e6fdf1973468ea172e8c36
Kévin Gomez <geek63@gmail.com>
2014-11-26 10:58:33 +0000

    Brand new web profiler interface

--- VCR/LoggedVideoRecorder.php
+++ VCR/LoggedVideoRecorder.php
@@ -2,6 +2,7 @@
 
 namespace KPhoen\VCRBundle\VCR;
 
+use Symfony\Component\HttpFoundation\Response;
 use VCR\Request;
 use VCR\Videorecorder as BaseRecorder;
 
@@ -17,11 +18,34 @@ class LoggedVideoRecorder extends BaseRecorder
         $response = parent::handleRequest($request);
 
         $this->log[] = array(
-            'request'  => $request->toArray(),
-            'response' => $response->toArray(),
+            'request'  => $this->augmentRequest($request->toArray()),
+            'response' => $this->augmentResponse($response->toArray()),
         );
 
         return $response;
+    }
+
+    private function augmentRequest(array $request)
+    {
+        $url_info = parse_url($request['url']);
+        $query_string = array();
+        parse_str(!empty($url_info['query']) ? $url_info['query'] : '', $query_string);
+
+        return array_merge(
+            $url_info,
+            array('query' => $query_string),
+            $request
+        );
+    }
+
+    private function augmentResponse(array $response)
+    {
+        $status_text = isset(Response::$statusTexts[$response['status']]) ? Response::$statusTexts[$response['status']] : 'Unknown';
+
+        return array_merge(
+            array('status_text' => $status_text),
+            $response
+        );
     }
 
     public function getLog()
