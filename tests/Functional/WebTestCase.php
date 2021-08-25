515a8983d4da3ce6ed6e84a157f4d74c3bca30e0
Daniel Hürtgen <daniel@higidi.de>
2021-08-25 14:20:24 +0200

    First functional test case added

---
+++ tests/Functional/WebTestCase.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace VCR\VCRBundle\Tests\Functional;
+
+use Neutron\TemporaryFilesystem\Manager;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as BaseWebTest;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\KernelInterface;
+use VCR\VCRBundle\DependencyInjection\VCRVCRExtension;
+use VCR\VCRBundle\Tests\Functional\App\Kernel;
+
+class WebTestCase extends BaseWebTest
+{
+    protected static $temporaryDirectoryManager;
+    protected static $configSubFolder;
+
+    protected static function getTemporaryDirectoryManager(): Manager
+    {
+        if (null === static::$temporaryDirectoryManager) {
+            static::$temporaryDirectoryManager = Manager::create();
+        }
+
+        return static::$temporaryDirectoryManager;
+    }
+
+    protected static function getKernelClass(): string
+    {
+        return Kernel::class;
+    }
+
+    protected static function createKernel(array $options = []): KernelInterface
+    {
+        $class = static::getKernelClass();
+
+        return new $class(
+            $options['environment'] ?? 'test',
+            $options['debug'] ?? false,
+            static::getTemporaryDirectoryManager(),
+            $options['config_sub_folder'] ?? static::$configSubFolder,
+            $options['additional_configs'] ?? []
+        );
+    }
+
+    protected function getAdditionalConfigurationLoaderCallable(
+        array $config,
+        string $extensionName = VCRVCRExtension::ALIAS
+    ): callable {
+        return function (ContainerBuilder $container) use ($config, $extensionName) {
+            $container->loadFromExtension($extensionName, $config);
+        };
+    }
+}
